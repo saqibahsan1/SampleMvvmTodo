b3c879ed04a0ac988cc95876f1cc2569128691d0
saqibahsan <s.ahsan@tawkeel.com>
2023-01-06 20:50:31 +0500

    added retrofit module

--- app/src/main/java/com/androar/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/androar/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -1,13 +1,13 @@
 package com.androar.mvvmtodo.ui.tasks
 
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.androar.mvvmtodo.data.TaskDao
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flatMapLatest
+import javax.inject.Inject
 
-class TasksViewModel @ViewModelInject constructor(
+class TasksViewModel @Inject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
 
